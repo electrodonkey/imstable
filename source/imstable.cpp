1cab87bc48a4f3db33a094132e1a3125bd1a1685
Dan Chirciu <dan.chirciu@gmail.com>
2021-02-10 02:57:55 +0200

    Add input parameter for file handling

--- source/imstable.cpp
+++ source/imstable.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
 #include <fstream>
 #include "spdlog/spdlog.h"
 
-int main() {
-  std::cout << "Hello World!\n";
-  spdlog::info("Welcome to spdlog!");
-  std::string path = "assets//lena.ascii.pgm";
+int main(int argc, char** argv) {
+  spdlog::info("Welcome to imstable!");
+  if(argc < 2)
+  {
+    spdlog::error("No input parameters!");
+    return -1;
+  }
+  std::string path = argv[1];
   std::string line;
   std::ifstream myfile (path);
   if (myfile.is_open())
@@ -14,18 +17,27 @@ int main() {
     std::getline(myfile,line);
     if(line.compare("P2") != 0)
     {
-      std::cerr << "Version error" << std::endl;
+      spdlog::error("Unexpected file type!");
+      throw;
     }
     else
     { 
-      std::cout << "Version : " << line << std::endl;
-    }
+      spdlog::info("File version:"+line);
+    }  
+      // Second line : comment
+      std::getline(myfile,line);
+      spdlog::info("Comment: "+line);
+
     // while ( getline (myfile,line) )
     // {
     //   std::cout << line << '\n';
     // }
     myfile.close();
   }
-  else std::cout << "Unable to open file"; 
+  else
+  {
+    spdlog::error("Unable to open file");
+    throw;
+  }
   return 0;
 } 
